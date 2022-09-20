d99e6c88e291ad7e87eae3fb5d3cd38cc62436b5
JYNi16 <49049752+JYNi16@users.noreply.github.com>
2022-09-20 23:12:52 +0800

    Add files via upload

--- PCA for 2DIsing model/sklearn_pca.py
+++ PCA for 2DIsing model/sklearn_pca.py
@@ -15,7 +15,6 @@ def load_data(root_path):
     for i in range(len(data_list)):
         color_z.append(float((data_list[i].split("\\")[-1]).split("_")[0]) / 10)
 
-
     for i in range(1, len(data_list)):
         tmp_data = np.load(data_list[i])
         spin_m_data = np.concatenate((spin_m_data, tmp_data), axis=0)
@@ -29,6 +28,49 @@ def pca_data(data_path):
     low_data = pca.transform(spin_m_data)
 
     return low_data, color_z
+
+def pca_spin(data1, data2, data3, path1, path2, path3):
+    data_list1 = []
+    data_list2 = []
+    data_list3 = []
+    color_z = []
+    data_list1.extend(glob.glob(os.path.join(path1, "*_50.npy")))
+    data_list2.extend(glob.glob(os.path.join(path2, "*_50.npy")))
+    data_list3.extend(glob.glob(os.path.join(path3, "*_50.npy")))
+    pca = PCA(n_components=1)
+    pca.fit(data1)
+    v1 = pca.components_
+
+    pca = PCA(n_components=1)
+    pca.fit(data2)
+    v2 = pca.components_
+
+    pca = PCA(n_components=1)
+    pca.fit(data3)
+    v3 = pca.components_
+
+    for i in range(len(data_list1)):
+        color_z.append(float((data_list3[i].split("\\")[-1]).split("_")[0]) / 10)
+
+    m1, m2, m3 = [], [], []
+    for i in range(len(data_list1)):
+        tmp_data1 = np.reshape(np.load(data_list1[i]), [-1, 1])
+        m1.append(np.abs(np.dot(v1, tmp_data1)[0][0]/20))
+
+        tmp_data2 = np.reshape(np.load(data_list2[i]), [-1, 1])
+        m2.append(np.abs(np.dot(v2, tmp_data2)[0][0]/40))
+
+        tmp_data3 = np.reshape(np.load(data_list3[i]), [-1, 1])
+        m3.append(np.abs(np.dot(v3, tmp_data3)[0][0]/80))
+
+    print(color_z, m1)
+    plt.plot(color_z, m1, marker="o", label="20")
+    plt.plot(color_z, m2, marker="o", label="40")
+    plt.plot(color_z, m3, marker="o", label="80")
+    plt.xlabel("temperatue")
+    plt.ylabel("m")
+    plt.legend()
+    plt.show()
 
 def plot(path1, path2, path3):
     data1, color_1 = pca_data(path1)
@@ -64,4 +106,9 @@ if __name__=="__main__":
     root_path_1 = "E:/deeplearning/Unsupervised learning for Phys//PCA for 2DIsing model/20_L"
     root_path_2 = "E:/deeplearning/Unsupervised learning for Phys//PCA for 2DIsing model/40_L"
     root_path_3 = "E:/deeplearning/Unsupervised learning for Phys//PCA for 2DIsing model/80_L"
-    plot(root_path_1, root_path_2, root_path_3)
+
+    data1, _ = load_data(root_path_1)
+    data2, _ = load_data(root_path_2)
+    data3, _ = load_data(root_path_3)
+
+    pca_spin(data1, data2, data3, root_path_1, root_path_2, root_path_3)
